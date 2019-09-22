d03d3b599fb4e84501900fdcd7d99e3cb30e093b
MarcoYacoub <marcogen2@gmail.com>
2019-09-22 16:03:32 +0200

    Add Initial Bulk ChemiClean Task

---
+++ ChemiCleanTest/IProductsRepositoryTest.cs
@@ -0,0 +1,95 @@
+using ChemiClean.Models;
+using ChemiClean.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace ChemiCleanTest
+{
+    public class ProductRepository
+    {
+        [Fact]
+        public async System.Threading.Tasks.Task GetProductsWithNoFiltrationAsync()
+        {
+            var options = new DbContextOptionsBuilder<ChemiCleanContext>()
+                .UseSqlServer("Data Source=MARCO-PC\\MSSQLSERVER01;Initial Catalog=ChemiCleanTest;Persist Security Info=True;User ID=admin_1;Password=admin")
+                .Options;
+            var dbContext = new ChemiCleanContext(options);
+            
+            ProductsRepository productsRepository = new ProductsRepository(dbContext);
+            List<TblProduct> products = productsRepository.GetProducts();
+            int x = await dbContext.TblProduct.CountAsync();
+            Assert.Equal(products.Count, x);
+        }
+        [Fact]
+        public async System.Threading.Tasks.Task GetProductsWithFiltration_PageSizeButNoPageNumberAsync()
+        {
+            var options = new DbContextOptionsBuilder<ChemiCleanContext>()
+                .UseSqlServer("Data Source=MARCO-PC\\MSSQLSERVER01;Initial Catalog=ChemiCleanTest;Persist Security Info=True;User ID=admin_1;Password=admin")
+                .Options;
+            var dbContext = new ChemiCleanContext(options);
+
+            ProductsRepository productsRepository = new ProductsRepository(dbContext);
+            List<TblProduct> products = productsRepository.GetProducts("",null,20);
+            int x = await dbContext.TblProduct.CountAsync();
+            Assert.Equal(products.Count, x);
+        }
+        [Fact]
+        public void GetProductsWithFiltration_CaseSensitive()
+        {
+            var options = new DbContextOptionsBuilder<ChemiCleanContext>()
+                .UseSqlServer("Data Source=MARCO-PC\\MSSQLSERVER01;Initial Catalog=ChemiClean;Persist Security Info=True;User ID=admin_1;Password=admin")
+                .Options;
+            var dbContext = new ChemiCleanContext(options);
+
+            ProductsRepository productsRepository = new ProductsRepository(dbContext);
+            List<TblProduct> products_upper = productsRepository.GetProducts("A");
+            List<TblProduct> products_lower = productsRepository.GetProducts("a");
+            Assert.Equal(products_upper, products_lower);
+        }
+        [Fact]
+        public void GetProductsWithFiltration_PageNumberVeryBigNumber()
+        {
+            var options = new DbContextOptionsBuilder<ChemiCleanContext>()
+                .UseSqlServer("Data Source=MARCO-PC\\MSSQLSERVER01;Initial Catalog=ChemiClean;Persist Security Info=True;User ID=admin_1;Password=admin")
+                .Options;
+            var dbContext = new ChemiCleanContext(options);
+
+            ProductsRepository productsRepository = new ProductsRepository(dbContext);
+            List<TblProduct> products = productsRepository.GetProducts("",1000,1000,"");
+            Assert.NotNull(products);
+        }
+        [Fact]
+        public void GetProductsWithFiltration_KeyWordEmptyString()
+        {
+            var options = new DbContextOptionsBuilder<ChemiCleanContext>()
+                .UseSqlServer("Data Source=MARCO-PC\\MSSQLSERVER01;Initial Catalog=ChemiClean;Persist Security Info=True;User ID=admin_1;Password=admin")
+                .Options;
+            var dbContext = new ChemiCleanContext(options);
+
+            ProductsRepository productsRepository = new ProductsRepository(dbContext);
+            List<TblProduct> productsEmpty = productsRepository.GetProducts("");
+            List<TblProduct> productsAll = productsRepository.GetProducts();
+            Assert.Equal(productsEmpty, productsAll);
+        }
+        [Fact]
+        public void GetProductDocumentsLastModified_NonExistingIds()
+        {
+            var options = new DbContextOptionsBuilder<ChemiCleanContext>()
+                .UseSqlServer("Data Source=MARCO-PC\\MSSQLSERVER01;Initial Catalog=ChemiClean;Persist Security Info=True;User ID=admin_1;Password=admin")
+                .Options;
+            var dbContext = new ChemiCleanContext(options);
+
+            ProductsRepository productsRepository = new ProductsRepository(dbContext);
+            
+            List<TblProduct> productsAll = productsRepository.GetProducts().OrderBy(p=>p.Id).ToList();
+            var productDocumentAll = productsRepository.GetProductDocumentsLastModified(productsAll.Select(p=>p.Id).ToList());
+            var fakeIds=new List<int> { productDocumentAll.Last().Key, productsAll.Last().Id+1,productsAll.Last().Id+2 };
+            var productDocument=productsRepository.GetProductDocumentsLastModified(fakeIds);
+            Assert.Equal(1, productDocument.Count);
+        }
+    }
+}
---
+++ ChemiCleanTest/ProductsServiceTest.cs
@@ -0,0 +1,97 @@
+﻿using ChemiClean.Models;
+using ChemiClean.Repositories;
+using ChemiClean.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace ChemiCleanTest
+{
+    public class ProductsServiceTest
+    {
+        // Should Make sure to close connections to DB.
+        // There are a lot of other cases that should be covered.
+        [Fact]
+        public async System.Threading.Tasks.Task DownLoadFile_NonexitingFile()
+        {
+            var options = new DbContextOptionsBuilder<ChemiCleanContext>()
+                .UseSqlServer("Data Source=MARCO-PC\\MSSQLSERVER01;Initial Catalog=ChemiCleanTest;Persist Security Info=True;User ID=admin_1;Password=admin")
+                .Options;
+            var dbContext = new ChemiCleanContext(options);
+
+            ProductsRepository productsRepository = new ProductsRepository(dbContext);
+            ProductsService productsService = new ProductsService(productsRepository);
+            var NonExistingFile=await productsService.DownloadFile(0);
+            Assert.Null(NonExistingFile);
+        }
+        [Fact]
+        public async System.Threading.Tasks.Task DownLoadFile_NonexitingURL()
+        {
+            var options = new DbContextOptionsBuilder<ChemiCleanContext>()
+                .UseSqlServer("Data Source=MARCO-PC\\MSSQLSERVER01;Initial Catalog=ChemiCleanTest;Persist Security Info=True;User ID=admin_1;Password=admin")
+                .Options;
+            var dbContext = new ChemiCleanContext(options);
+
+            ProductsRepository productsRepository = new ProductsRepository(dbContext);
+            ProductsService productsService = new ProductsService(productsRepository);
+            var product=productsRepository.GetProducts().FirstOrDefault();
+            if (product!=null)
+            {
+                product.Url = "None";
+                productsRepository.Commit();
+            }
+            var NonExistingFile = await productsService.DownloadFile(product.Id);
+            Assert.Null(NonExistingFile);
+        }
+        [Fact]
+        public void GetProductById_NoneExistingId()
+        {
+            var options = new DbContextOptionsBuilder<ChemiCleanContext>()
+                .UseSqlServer("Data Source=MARCO-PC\\MSSQLSERVER01;Initial Catalog=ChemiCleanTest;Persist Security Info=True;User ID=admin_1;Password=admin")
+                .Options;
+            var dbContext = new ChemiCleanContext(options);
+
+            ProductsRepository productsRepository = new ProductsRepository(dbContext);
+            ProductsService productsService = new ProductsService(productsRepository);
+            
+            var NonExistingFile = productsService.GetProductById(0);
+            Assert.Null(NonExistingFile);
+        }
+        [Fact]
+        public async System.Threading.Tasks.Task CheckProductDataSheetAvailabilityAsync_NonExistingProduct()
+        {
+            var options = new DbContextOptionsBuilder<ChemiCleanContext>()
+                .UseSqlServer("Data Source=MARCO-PC\\MSSQLSERVER01;Initial Catalog=ChemiCleanTest;Persist Security Info=True;User ID=admin_1;Password=admin")
+                .Options;
+            var dbContext = new ChemiCleanContext(options);
+
+            ProductsRepository productsRepository = new ProductsRepository(dbContext);
+            ProductsService productsService = new ProductsService(productsRepository);
+
+            var NonExistingFile =await productsService.CheckProductDataSheetAvailabilityAsync(0);
+            Assert.False(NonExistingFile);
+        }
+        [Fact]
+        public async System.Threading.Tasks.Task CheckProductDataSheetAvailabilityAsync_NonExistingURL()
+        {
+            var options = new DbContextOptionsBuilder<ChemiCleanContext>()
+                .UseSqlServer("Data Source=MARCO-PC\\MSSQLSERVER01;Initial Catalog=ChemiCleanTest;Persist Security Info=True;User ID=admin_1;Password=admin")
+                .Options;
+            var dbContext = new ChemiCleanContext(options);
+
+            ProductsRepository productsRepository = new ProductsRepository(dbContext);
+            ProductsService productsService = new ProductsService(productsRepository);
+            var product = productsRepository.GetProducts().FirstOrDefault();
+            if (product != null)
+            {
+                product.Url = "None";
+                productsRepository.Commit();
+            }
+            var NonExistingFile = await productsService.CheckProductDataSheetAvailabilityAsync(product.Id);
+            Assert.False(NonExistingFile);
+        }
+    }
+}
